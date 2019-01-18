86e53e31e4a53aec46e51df8e9f4eabea77b86c0
赵金浩 <2481406659@qq.com>
2019-01-18 14:00:36 +0800

    构建基本框架

--- main.c
+++ main.c
@@ -1,8 +1,41 @@
 #include <stdio.h>
 
-int main(){
+int main()
+{
+    printf("jinruzhuancheguanlixitong\n");
+    
+    while (1)
+    {
+        printf("1---tianjiayigecheliangweizhixiaoxi\n");
+        printf("2---shanchuzuihouyigecheliangweizhixinxi\n");
+        printf("3---xianshisuoyoucheliangweizhixinxi\n");
+        printf("4---chazhaojulizuijindecheliang\n");
+        printf("5---tuichuxitong\n");
 
-    printf("hello world!");
+        printf("qingshurusuoxyxiangmushuzi\n");
+        int code;
+        scanf("%d",&code);
+
+        if (code ==1){
+
+        }
+        if (code ==2){
+            
+        }
+        if (code ==3){
+            
+        }
+        if (code ==4){
+            
+        }
+        if (code ==5){
+            printf("dianjihuiche,tuichuxitong\n");
+            break;
+            
+        }
+    }
+
+    
 
     return 0;
 
